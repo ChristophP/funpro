c9661f2a4abf8dc722fdaa8c8a164e479494e1ca
ChristophP <deedop@hotmail.de>
2019-10-15 23:12:53 +0200

    add script for running tests in node 4, 6, 8 and 10. Only node 4 fails

---
+++ test-node-version.sh
@@ -0,0 +1,13 @@
+VOLUMES="-v $(pwd)/index.js:/test/index.js -v $(pwd)/index.test.js:/test/index.test.js -v $(pwd)/package.json:/test/package.json"
+
+test()
+{
+  VERSION=$1
+  echo "TESTING WITH NODE VERSION ${VERSION}";
+  docker run -it ${VOLUMES} -w "/test" node:${VERSION}-alpine sh -c "npm i && npm test";
+}
+
+test 4
+test 6
+test 8
+test 10
