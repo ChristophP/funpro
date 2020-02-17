897b6e0375d86af0f6b092da1c29e36b9d1bc009
ChristophP <deedop@hotmail.de>
2020-02-17 14:52:40 +0100

    run tests with jest, remove mocha, chai, etc.

--- index.test.js
+++ index.test.js
@@ -1,10 +1,6 @@
 const R = require('ramda');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const expect = chai.expect;
-chai.use(chaiAsPromised);
 
-const { union, matchWith, Maybe, Result, Task } = require('./index');
+const {union, matchWith, Maybe, Result, Task} = require('./index');
 
 const unionEquals = val1 => val2 => val1.equals(val2);
 
@@ -15,76 +11,76 @@ describe('union', () => {
   });
 
   describe('create', () => {
-    it('a Something has the correct tag.', () => {
-      expect(Perhaps.Something(42)).to.include({
+    test('a Something has the correct tag.', () => {
+      expect(Perhaps.Something(42)).toEqual(expect.objectContaining({
         __ctor: 'Something',
-      });
+      }));
     });
 
-    it('matches a Something with a value.', () => {
+    test('matches a Something with a value.', () => {
       const perhaps = Perhaps.Something(42);
       const matched = matchWith(perhaps, {
         Something: val => val,
         None: Function.prototype,
       });
-      expect(matched).to.equal(42);
+      expect(matched).toEqual(42);
     });
 
-    it('a None has the correct tag.', () => {
-      expect(Perhaps.None()).to.include({
+    test('a None has the correct tag.', () => {
+      expect(Perhaps.None()).toEqual(expect.objectContaining({
         __ctor: 'None',
-      });
+      }));
     });
 
-    it('matches a None without any values.', () => {
+    test('matches a None without any values.', () => {
       const perhaps = Perhaps.None();
       const matched = matchWith(perhaps, {
         Something: Function.prototype,
         None: () => 'I am none',
       });
-      expect(matched).to.equal('I am none');
+      expect(matched).toEqual('I am none');
     });
 
-    it('throws when matching a non-union type.', () => {
+    test('throws when matching a non-union type.', () => {
       const badFn = () => matchWith('not a union', {});
-      expect(badFn).to.throw();
+      expect(badFn).toThrow();
     });
 
-    it('creates curried constructors', () => {
-      const Point = union({ Point: 2 });
-      const Color = union({ RGB: 3 });
-      const Bool = union({ True: 0, False: 0 });
-      expect(Color.RGB(255, 0, 0)).to.satisfy(
+    test('creates curried constructors', () => {
+      const Point = union({Point: 2});
+      const Color = union({RGB: 3});
+      const Bool = union({True: 0, False: 0});
+      expect(Color.RGB(255, 0, 0)).toSatisfy(
         unionEquals(Color.RGB(255)(0)(0))
       );
-      expect(Point.Point(1, 0)).to.satisfy(unionEquals(Point.Point(1)(0)));
-      expect(Bool.True()).to.satisfy(unionEquals(Bool.True()));
+      expect(Point.Point(1, 0)).toSatisfy(unionEquals(Point.Point(1)(0)));
+      expect(Bool.True()).toSatisfy(unionEquals(Bool.True()));
     });
   });
 
   describe('prototype', () => {
-    it('Something and None have the same constructor.', () => {
+    test('Something and None have the same constructor.', () => {
       const none = Perhaps.None();
       const something = Perhaps.Something(42);
-      expect(none.constructor).to.equal(something.constructor);
+      expect(none.constructor).toEqual(something.constructor);
     });
 
-    it('Maybe and Perhaps have different prototypes.', () => {
-      expect(Perhaps.prototype).not.to.equal(Maybe.prototype);
+    test('Maybe and Perhaps have different prototypes.', () => {
+      expect(Perhaps.prototype).not.toEqual(Maybe.prototype);
     });
 
     describe('equals', () => {
       const something = Perhaps.Something(42);
-      it('should equal when the value is the same', () => {
-        expect(something).to.satisfy(unionEquals(Perhaps.Something(42)));
+      test('should equal when the value is the same', () => {
+        expect(something).toSatisfy(unionEquals(Perhaps.Something(42)));
       });
 
-      it('should not be equal when the value is the different', () => {
-        expect(something).not.to.satisfy(unionEquals(Perhaps.Something(3)));
+      test('should not be equal when the value is the different', () => {
+        expect(something).not.toSatisfy(unionEquals(Perhaps.Something(3)));
       });
 
-      it('should not be equal when the tag is the different', () => {
-        expect(something).not.to.satisfy(unionEquals(Perhaps.None()));
+      test('should not be equal when the tag is the different', () => {
+        expect(something).not.toSatisfy(unionEquals(Perhaps.None()));
       });
     });
   });
@@ -95,89 +91,89 @@ describe('union', () => {
       Nothing: () => 'default',
     };
 
-    it('works on a Just value', () => {
-      expect(matchWith(Maybe.Just('Hello'), cases)).to.equal('Hello');
+    test('works on a Just value', () => {
+      expect(matchWith(Maybe.Just('Hello'), cases)).toEqual('Hello');
     });
 
-    it('works on a Nothing value', () => {
-      expect(matchWith(Maybe.Nothing(), cases)).to.equal('default');
+    test('works on a Nothing value', () => {
+      expect(matchWith(Maybe.Nothing(), cases)).toEqual('default');
     });
   });
 });
 
 describe('Maybe', () => {
-  it('is Just in the minimum default context', () => {
-    expect(Maybe.of(3)).to.satisfy(unionEquals(Maybe.Just(3)));
+  test('is Just in the minimum default context', () => {
+    expect(Maybe.of(3)).toSatisfy(unionEquals(Maybe.Just(3)));
   });
 
   describe('Just', () => {
-    it('creates a Just value', () => {
+    test('creates a Just value', () => {
       const maybe = Maybe.Just(42);
-      expect(maybe).to.deep.include({
+      expect(maybe).toEqual(expect.objectContaining({
         __ctor: 'Just',
         __args: [42],
-      });
+      }));
     });
   });
 
   describe('Nothing', () => {
-    it('creates a Nothing value', () => {
+    test('creates a Nothing value', () => {
       const maybe = Maybe.Nothing();
-      expect(maybe).to.deep.include({
+      expect(maybe).toEqual(expect.objectContaining({
         __ctor: 'Nothing',
         __args: [],
-      });
+      }));
     });
   });
 
   describe('map', () => {
     const double = R.multiply(2);
-    it("maps when it's just", () => {
+    test("maps when it's just", () => {
       const maybe = Maybe.Just(10);
-      expect(maybe.map(double)).to.satisfy(unionEquals(Maybe.Just(20)));
+      expect(maybe.map(double)).toSatisfy(unionEquals(Maybe.Just(20)));
     });
 
-    it("skips when it's nothing", () => {
+    test("skips when it's nothing", () => {
       const maybe = Maybe.Nothing();
-      expect(maybe.map(double)).to.satisfy(unionEquals(maybe));
+      expect(maybe.map(double)).toSatisfy(unionEquals(maybe));
     });
   });
 
   describe('chain', () => {
     const listHead = list =>
       list.length === 0 ? Maybe.Nothing() : Maybe.Just(list[0]);
-    it('maps and flattens', () => {
+    test('maps and flattens', () => {
       const maybe = Maybe.Just([1, 2]);
-      expect(maybe.chain(listHead)).to.satisfy(unionEquals(Maybe.Just(1)));
+      expect(maybe.chain(listHead)).toSatisfy(unionEquals(Maybe.Just(1)));
     });
 
-    it("skips when it's nothing", () => {
+    test("skips when it's nothing", () => {
       const maybe = Maybe.Nothing();
-      expect(maybe.chain(listHead)).to.satisfy(unionEquals(Maybe.Nothing())).to;
+      expect(maybe.chain(listHead)).toSatisfy(unionEquals(Maybe.Nothing()));
     });
   });
 
   describe('ap', () => {
-    it('applies the argument to a function', () => {
+    test('applies the argument to a function', () => {
       const add = a => b => a + b;
       const maybeFn = Maybe.Just(add);
-      expect(maybeFn.ap(Maybe.Just(3)).ap(Maybe.Just(2))).to.satisfy(
+      expect(maybeFn.ap(Maybe.Just(3)).ap(Maybe.Just(2))).toSatisfy(
         unionEquals(Maybe.Just(5))
       );
     });
 
-    it('does nothing if function is Nothing', () => {
+    test('does nothing if function is Nothing', () => {
       const add = a => b => a + b;
       const maybeFn = Maybe.Nothing();
-      expect(maybeFn.ap(Maybe.Just(3)).ap(Maybe.Just(2))).to.satisfy(
+      expect(maybeFn.ap(Maybe.Just(3)).ap(Maybe.Just(2))).toSatisfy(
         unionEquals(Maybe.Nothing())
       );
     });
 
-    it('does nothing if value is Nothing', () => {
+    test('does nothing if value is Nothing', () => {
       const add = a => b => a + b;
       const maybeFn = Maybe.Just(add);
-      expect(maybeFn.ap(Maybe.Nothing())).to.satisfy(
+      expect(maybeFn.ap(Maybe.Nothing())).toSatisfy(
         unionEquals(Maybe.Nothing())
       );
     });
@@ -190,28 +186,28 @@ describe('Result', () => {
     Ok: val => val,
   };
 
-  it('matches Err', () => {
+  test('matches Err', () => {
     const result = Result.Err('I am err');
-    expect(matchWith(result, cases)).to.equal('I am err');
+    expect(matchWith(result, cases)).toEqual('I am err');
   });
 
-  it('matches Ok', () => {
+  test('matches Ok', () => {
     const result = Result.Ok('I am ok');
-    expect(matchWith(result, cases)).to.equal('I am ok');
+    expect(matchWith(result, cases)).toEqual('I am ok');
   });
 
-  it('is Ok in the minimum default context', () => {
-    expect(Result.of(3)).to.satisfy(unionEquals(Result.Ok(3)));
+  test('is Ok in the minimum default context', () => {
+    expect(Result.of(3)).toSatisfy(unionEquals(Result.Ok(3)));
   });
 
   describe('.map', () => {
     const double = val => 2 * val;
-    it("maps when it's Ok", () => {
-      expect(Result.Ok(3).map(double)).to.satisfy(unionEquals(Result.Ok(6)));
+    test("maps when it's Ok", () => {
+      expect(Result.Ok(3).map(double)).toSatisfy(unionEquals(Result.Ok(6)));
     });
 
-    it("skips when it's Err", () => {
-      expect(Result.Err('Oh no').map(double)).to.satisfy(
+    test("skips when it's Err", () => {
+      expect(Result.Err('Oh no').map(double)).toSatisfy(
         unionEquals(Result.Err('Oh no'))
       );
     });
@@ -219,14 +215,14 @@ describe('Result', () => {
 
   describe('.mapError', () => {
     const toUpper = str => str.toUpperCase();
-    it("skips when it's Ok", () => {
-      expect(Result.Ok(3).mapError(toUpper)).to.satisfy(
+    test("skips when it's Ok", () => {
+      expect(Result.Ok(3).mapError(toUpper)).toSatisfy(
         unionEquals(Result.Ok(3))
       );
     });
 
-    it("maps the error when it's Err", () => {
-      expect(Result.Err('Oh no').mapError(toUpper)).to.satisfy(
+    test("maps the error when it's Err", () => {
+      expect(Result.Err('Oh no').mapError(toUpper)).toSatisfy(
         unionEquals(Result.Err('OH NO'))
       );
     });
@@ -240,40 +236,40 @@ describe('Result', () => {
         return Result.Err(e);
       }
     };
-    it("maps and flattens when it's Ok", () => {
-      expect(Result.Ok('{ "a": 3 }').chain(safeJsonParse)).to.satisfy(
-        unionEquals(Result.Ok({ a: 3 }))
+    test("maps and flattens when it's Ok", () => {
+      expect(Result.Ok('{ "a": 3 }').chain(safeJsonParse)).toSatisfy(
+        unionEquals(Result.Ok({a: 3}))
       );
     });
 
-    it("skips when it's Err", () => {
-      expect(Result.Err('Oh no').chain(safeJsonParse)).to.satisfy(
+    test("skips when it's Err", () => {
+      expect(Result.Err('Oh no').chain(safeJsonParse)).toSatisfy(
         unionEquals(Result.Err('Oh no'))
       );
     });
   });
 
   describe('.ap', () => {
-    it('applies the argument to a function', () => {
+    test('applies the argument to a function', () => {
       const add = a => b => a + b;
       const resultFn = Result.Ok(add);
-      expect(resultFn.ap(Result.Ok(3)).ap(Result.Ok(2))).to.satisfy(
+      expect(resultFn.ap(Result.Ok(3)).ap(Result.Ok(2))).toSatisfy(
         unionEquals(Result.Ok(5))
       );
     });
 
-    it('does nothing if function is Err', () => {
+    test('does nothing if function is Err', () => {
       const add = a => b => a + b;
       const resultFn = Result.Err('Oh no');
-      expect(resultFn.ap(Result.Ok(3)).ap(Result.Ok(2))).to.satisfy(
+      expect(resultFn.ap(Result.Ok(3)).ap(Result.Ok(2))).toSatisfy(
         unionEquals(Result.Err('Oh no'))
       );
     });
 
-    it('does nothing if value is Err', () => {
+    test('does nothing if value is Err', () => {
       const add = a => b => a + b;
       const resultFn = Result.Ok(add);
-      expect(resultFn.ap(Result.Err('Oh no'))).to.satisfy(
+      expect(resultFn.ap(Result.Err('Oh no'))).toSatisfy(
         unionEquals(Result.Err('Oh no'))
       );
     });
@@ -282,103 +278,103 @@ describe('Result', () => {
 
 describe('Task', () => {
   describe('.succeed', () => {
-    it('creates a successful task', () => {
+    test('creates a successful task', () => {
       const task = Task.succeed(3);
-      return expect(task.run()).to.eventually.equal(3);
+      return expect(task.run()).resolves.toBe(3);
     });
   });
 
   describe('.failed', () => {
-    it('creates a failed task', () => {
+    test('creates a failed task', () => {
       const task = Task.fail('OMG');
-      return expect(task.run()).to.be.rejectedWith('OMG');
+      return expect(task.run()).rejects.toBe('OMG');
     });
   });
 
   describe('.all', () => {
-    it('batches tasks', () => {
+    test('batches tasks', () => {
       const task = Task.all([Task.succeed(3), Task.succeed(5)]);
-      return expect(task.run()).to.eventually.deep.equal([3, 5]);
+      return expect(task.run()).resolves.toEqual([3, 5]);
     });
 
-    it('fails if one of them fails', () => {
+    test('fails if one of them fails', () => {
       const task = Task.all([Task.succeed(3), Task.fail('Oh no')]);
-      return expect(task.run()).to.eventually.be.rejectedWith('Oh no');
+      return expect(task.run()).rejects.toBe('Oh no');
     });
 
-    it('can be mapped like any task', () => {
+    test('can be mapped like any task', () => {
       const sum = ([a, b]) => a + b;
       const task = Task.all([Task.succeed(3), Task.succeed(5)]).map(sum);
-      return expect(task.run()).to.eventually.equal(8);
+      return expect(task.run()).resolves.toBe(8);
     });
   });
 
   describe('.sequence', () => {
-    it('executes tasks in sequence', () => {
+    test('executes tasks in sequence', () => {
       const task = Task.sequence([Task.succeed(3), Task.succeed(5)]);
-      return expect(task.run()).to.eventually.deep.equal([3, 5]);
+      return expect(task.run()).resolves.toEqual([3, 5]);
     });
 
-    it('executes tasks in order from left to right', () => {
+    test('executes tasks in order from left to right', () => {
       const delayTask = Task.of(
         () => new Promise(resolve => setTimeout(resolve, 500))
       ).map(_ => 3);
 
       const task = Task.sequence([delayTask, Task.succeed(5)]);
-      return expect(task.run()).to.eventually.deep.equal([3, 5]);
+      return expect(task.run()).resolves.toEqual([3, 5]);
     });
 
-    it('fails if one of them fails', () => {
+    test('fails if one of them fails', () => {
       const task = Task.sequence([Task.succeed(3), Task.fail('Oh no')]);
-      return expect(task.run()).to.eventually.be.rejectedWith('Oh no');
+      return expect(task.run()).rejects.toBe('Oh no');
     });
   });
 
   describe('.prototype', () => {
     describe('.map', () => {
-      it('maps when successful', () => {
+      test('maps when successful', () => {
         const double = a => a * 2;
         const task = Task.succeed(42).map(double);
-        return expect(task.run()).to.eventually.equal(84);
+        return expect(task.run()).resolves.toBe(84);
       });
     });
 
     describe('.map2', () => {
-      it('maps when successful', () => {
+      test('maps when successful', () => {
         const add = (a, b) => a + b;
         const task = Task.succeed(42).map2(add, Task.succeed(3));
-        return expect(task.run()).to.eventually.equal(45);
+        return expect(task.run()).resolves.toBe(45);
       });
 
-      it('fails when one of them fails', () => {
+      test('fails when one of them fails', () => {
         const add = (a, b) => a + b;
         const task = Task.succeed(42).map2(add, Task.fail('Oh no'));
-        return expect(task.run()).to.eventually.be.rejectedWith('Oh no');
+        return expect(task.run()).rejects.toBe('Oh no');
       });
     });
 
     describe('.mapError', () => {
-      it('maps the error when failed', () => {
+      test('maps the error when failed', () => {
         const toLower = str => str.toLowerCase();
         const task = Task.fail('OH NO').mapError(toLower);
-        return expect(task.run()).to.be.rejectedWith('oh no');
+        return expect(task.run()).rejects.toBe('oh no');
       });
     });
 
     describe('.chain', () => {
-      it('maps and flattens when successful', () => {
+      test('maps and flattens when successful', () => {
         const task = Task.succeed('Hello');
         const toConcatTask = val => Task.of(() => val.concat('!!!'));
         const chainedTask = task.chain(toConcatTask);
-        return expect(chainedTask.run()).to.eventually.equal('Hello!!!');
+        return expect(chainedTask.run()).resolves.toBe('Hello!!!');
       });
     });
 
     describe('.onError', () => {
-      it('catches and flattens when failed', () => {
+      test('catches and flattens when failed', () => {
         const toConcatTask = val => Task.of(() => val.concat('!!!'));
         const task = Task.fail('Oh no').onError(toConcatTask);
-        return expect(task.run()).to.eventually.equal('Oh no!!!');
+        return expect(task.run()).resolves.toBe('Oh no!!!');
       });
     });
   });
