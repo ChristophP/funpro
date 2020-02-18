3635501d786ba5b8d4aa4a9270380ff6d74686e3
ChristophP <deedop@hotmail.de>
2020-02-18 22:53:11 +0100

    adjust tests for node version to later version

--- test-node-version.sh
+++ test-node-version.sh
@@ -1,4 +1,4 @@
-VOLUMES="-v $(pwd)/index.js:/test/index.js -v $(pwd)/index.test.js:/test/index.test.js -v $(pwd)/package.json:/test/package.json"
+VOLUMES="-v $(pwd)/index.js:/test/index.js -v $(pwd)/index.test.js:/test/index.test.js -v $(pwd)/package.json:/test/package.json -v $(pwd)/jest.config.js:/test/jest.config.js"
 
 test()
 {
@@ -7,7 +7,8 @@ test()
   docker run -it ${VOLUMES} -w "/test" node:${VERSION}-alpine sh -c "npm i && npm test";
 }
 
-test 4
+#test 4
 test 6
 test 8
 test 10
+test 12
